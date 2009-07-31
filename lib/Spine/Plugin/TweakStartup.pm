6c8c2af78ddb1ecc74d7bdf05f98f80eac9bf7fb
cfb <cfb@6b84982a-6eae-4b3d-bba8-bb687524a50d>
2009-07-31 22:16:09 +0000

    Fix for issue #66.
    Add support to TweakStartup for managing xinetd services on redhat
    using chkconfig.

--- lib/Spine/Plugin/TweakStartup.pm
+++ lib/Spine/Plugin/TweakStartup.pm
@@ -28,8 +28,8 @@ use Spine::Util qw(exec_initscript);
 
 our ($VERSION, $DESCRIPTION, $MODULE);
 
-$VERSION = sprintf("%d", q$Revision$ =~ /(\d+)/);
-$DESCRIPTION = "A variety of changes to tweak the boot process for the machine";
+$VERSION = sprintf('%d', q$Revision$ =~ /(\d+)/);
+$DESCRIPTION = 'A variety of changes to tweak the boot process for the machine';
 
 $MODULE = { author => 'osscode@ticketmaster.com',
             description => $DESCRIPTION,
@@ -38,84 +38,99 @@ $MODULE = { author => 'osscode@ticketmaster.com',
                                     code => \&tweak_startup } ] }
           };
 
-
 use File::Basename;
 
 my ($DRYRUN, $CHKCONFIG);
-
 
 sub tweak_startup
 {
     my $c = shift;
     my $l = shift;
 
-    if (ref($l) == 'Spine::State') {
+    if (ref($l) == 'Spine::State')
+    {
         $l = $l->data();
     }
 
     my $rval = 0;
 
     my $init_dir = $c->getval('init_dir');
+    my $inetd_dir = $c->getval('inetd_dir');
 
     # Action specific values
     my $stop = $c->getval('startup_execstop');
     my $services_ignore = $c->getvals('startup_ignore');
     my $services_on = $c->getvals('startup');
 
+    # Scripts we will process
+    my @valid_scripts;
+
     # Data from the previous run
     my $last_services_on = [];
-    if (defined($l)) {
+    if (defined($l)) 
+    {
         $last_services_on = $l->getvals('startup');
     }
 
     $DRYRUN = $c->getval('c_dryrun');
     $CHKCONFIG = $c->getval('chkconfig_bin');
 
-    foreach my $startscript (<$init_dir/*>)
+    # Build a list of services if the directories exist.
+    foreach my $dir ($init_dir, $inetd_dir)
     {
-        next unless (-x "$startscript");
+        if ($dir) 
+        {
+            foreach my $script (<$dir/*>)
+            {
+                next unless (-e "$script");
+                push(@valid_scripts, $script);
+            }
+        }
+    }
+
+    foreach my $startscript (@valid_scripts)
+    {
         my $service = basename($startscript);
 
-	# We will not try to turn off these somewhat special
+        # We will not try to turn off these somewhat special
         # scripts in /etc/init.d/.
-        next if ( grep(/$service/, @{$services_ignore}) );
+        next if (grep(/$service/, @{$services_ignore}));
 
         my $status = 'off';
-        $status = 'on' if ( grep(/^${service}$/, @{$services_on}) );
-
-	# Stop daemons that are not supposed to be running.
-	if ( ($status eq 'off') and ($stop)
-                and (is_service_enabled($c, $service)) )
-	{
-	    $c->cprint("stopping $service", 2);
-            exec_initscript($c, $service, "stop", 0);
-	}
+        $status = 'on' if (grep(/^${service}$/, @{$services_on}));
 
         # Manipulate our runlevels.
-        if ( ((is_service_enabled($c, $service)) and ($status =~ "off")) or
-           ((not is_service_enabled($c, $service)) and ($status =~ "on")) )
+        if (((is_service_enabled($c, $service)) and ($status =~ 'off')) or
+            ((not is_service_enabled($c, $service)) and ($status =~ 'on')))
         {
             $c->cprint("turning $status $service", 2);
-
             config_init($c, $service, $status) or $rval++;
         }
 
+        next if $startscript =~ m/$inetd_dir/;
 
-	# Start services that have been turned on since the last time we ran
-	if (($status eq "on") and (scalar @{$last_services_on} >= 1 ))
-	{
-	    if ( (not grep(/^${service}$/, @{$last_services_on})) and
-		 (grep(/^${service}$/, @{$services_on})) )
-	    {
-		$c->cprint("starting $service", 2);
-                exec_initscript($c, $service, "start", 1) or $rval++;
-	    }
-	}
+        # Stop daemons that are not supposed to be running.
+        if (($status eq 'off') and ($stop) and
+            (is_service_enabled($c, $service)))
+        {
+            $c->cprint("stopping $service", 2);
+            exec_initscript($c, $service, 'stop', 0);
+        }
+
+        # Start services that have been turned on since the last time we ran
+        if (($status eq 'on') and (scalar @{$last_services_on} >= 1 ))
+        {
+            if ((not grep(/^${service}$/, @{$last_services_on})) and
+                (grep(/^${service}$/, @{$services_on})))
+            {
+                $c->cprint("starting $service", 2);
+                exec_initscript($c, $service, 'start', 1) or $rval++;
+            }
+        }
     }
 
     return $rval ? PLUGIN_ERROR : PLUGIN_SUCCESS;
 }
-
 
 sub config_init
 {
@@ -125,26 +140,27 @@ sub config_init
     my $result = `$CHKCONFIG $service $status 2>&1`;
     if ($result)
     {
-        $c->error("failed for $service [$result]", "err");
+        $c->error("failed for $service [$result]", 'err');
         return 0;
     }
     return 1;
 }
 
-
 sub is_service_enabled
 {
     my ($c, $service) = @_;
-
     my $chkconfig_output = `$CHKCONFIG --list $service 2>/dev/null`;
+    my @service_status = split /\s+/, $chkconfig_output;
 
-    # We only want to deal with runlevels 3-5
-    my (undef, undef, undef, undef, @service_status)
-        = split /\s+/, $chkconfig_output;
-
-    return 1 if grep /\d+:on/, @service_status;
+    if ($#service_status > 2) {
+        # "traditional" output: we only want to deal with runlevels 3-5
+        splice @service_status, 0, 4;
+        return 1 if grep /\d+:on/, @service_status;
+    } else {
+        # xinetd output
+        return 1 if $service_status[1] eq 'on';
+    }
     return 0;
 }
-
 
 1;
